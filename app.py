2f9cc4d70a2674d37dbc2f408188c6ecf8136879
Demos Petsas <demos@petsas.consulting>
2023-03-20 11:07:39 +0200

    remove blueprint

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask
 
-from api.views import blueprint
-
 app = Flask(__name__)
-app.register_blueprint(blueprint=blueprint)
 app.config.from_object("config")
 
 print(f"The app environment is: {app.config.get('ENV')}")
